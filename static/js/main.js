fc0ba92137bb25d9a29923deca8d4b9c56de459c
Holly Horwood <holly.horwood@live.com>
2019-08-05 12:52:27 +1200

    added login modal

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 //Original code courtesy of nkon on Stack Overflow https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
-  var previewFile = function(event) {
-    var output = document.getElementById('output');
+  const previewFile = function(event) {
+    let output = document.getElementById('output');
     output.src = URL.createObjectURL(event.target.files[0]);
   };
 
