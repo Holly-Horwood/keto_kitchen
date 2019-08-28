08f8fb75d699389b6cf935675db2c6ecf5424b07
Holly Horwood <holly.horwood@live.com>
2019-08-29 09:19:34 +1200

    removed config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config(object):
-
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    #MONGO_URI = os.environ.get('MONGO_URI')
-    MONGO_URI = "mongodb+srv://nz_user:Reef5115@myfirstcluster-clbrq.mongodb.net/keto_kitchen?retryWrites=true&w=majority"
-    
