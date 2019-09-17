abaa00f441bbff008197782909d32a1107ab4610
Holly Horwood <holly.horwood@live.com>
2019-09-17 17:00:53 +1200

    Added some more recipes and added per serve note to add recipe page

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
-import unittest
-from app import search_recipes
+import unittest 
+from app import search_recipes, app
+from flask import Flask
+
 
 class SearchTests(unittest.TestCase):
 
@@ -34,12 +36,21 @@ class SearchTests(unittest.TestCase):
         assert len(results) != 0 and "Keto" in results[0]["diet"], "Should be at least one Keto recipe"      
 
 class WebTests(unittest.TestCase):
+    
     def setUp(self):
-        pass
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.client = app.test_client()
 
     def tearDown(self):
         pass 
 
+# Test that the add_recipe page can be found
+    def test_that_add_recipe_page_found(self):
+        response = app.test_client(self).get('/add_recipe', follow_redirects=True)
+        self.assertEqual(response.status_code, 200, "add_recipe returned status code %s should be 200" % str(response.status_code)) 
+ # Coph abpve and try for different pages, id will be needed for edit and full recipe ie '/edit_recipe/52156632566'  TODO: to run test python test.py
 
 
 if __name__=="__main__":
